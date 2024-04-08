f12fae0fb2c8f4f6f3a9cf66180a3f8d1844c300
takesei <sei06k14@gmail.com>
2024-04-08 19:47:51 +0900

    :sparkles: pass builder3

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,6 +22,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
             env: Option<Vec<String>>,
             current_dir: Option<String>,
         }
+
+       impl CommandBuilder {
+           fn executable(&mut self, executable: String) -> &mut Self {
+               self.executable = Some(executable);
+               self
+           }
+           fn args(&mut self, args: Vec<String>) -> &mut Self {
+               self.args = Some(args);
+               self
+           }
+           fn env(&mut self, env: Vec<String>) -> &mut Self {
+               self.env = Some(env);
+               self
+           }
+           fn current_dir(&mut self, current_dir: String) -> &mut Self {
+               self.current_dir = Some(current_dir);
+               self
+           }
+       }
+
+
         impl #name {
             fn builder() -> CommandBuilder {
                 CommandBuilder {
