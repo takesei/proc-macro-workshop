eefba1cd67a585862f71e70c445d099a7a8cff96
takesei <sei06k14@gmail.com>
2024-04-08 19:18:40 +0900

    :sparkles: pass builder test 1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,16 @@
 use proc_macro::TokenStream;
+use quote::quote;
+
+// pub struct DeriveInput {
+//    pub attrs: Vec<Attribute>,
+//    pub vis: Visibility,
+//    pub ident: Ident,
+//    pub generics: Generics,
+//    pub data: Data,
+//}
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(_: TokenStream) -> TokenStream {
+    let expanded = quote! {};
+    proc_macro::TokenStream::from(expanded)
 }
