67091b04063c34aa565236db79523bf3ce06ce2b
takesei <sei06k14@gmail.com>
2024-04-08 19:40:38 +0900

    :sparkles: pass builder2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,5 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 // pub struct DeriveInput {
 //    pub attrs: Vec<Attribute>,
@@ -10,7 +11,27 @@ use quote::quote;
 //}
 
 #[proc_macro_derive(Builder)]
-pub fn derive(_: TokenStream) -> TokenStream {
-    let expanded = quote! {};
+pub fn derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None
+                }
+            }
+        }
+    };
     proc_macro::TokenStream::from(expanded)
 }
